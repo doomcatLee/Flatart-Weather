768eaf1983b53e1faf8678889b3cfe76cb20ac88
Guest User <Guest@Epicodus-5B.local>
2017-05-31 11:39:20 -0700

    add timestamp and retreive results

---
+++ app/src/main/java/com/example/guest/weatherandroid/adapters/ForecastListAdapter.java
@@ -0,0 +1,62 @@
+package com.example.guest.weatherandroid.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.guest.weatherandroid.Model.Weather;
+import com.example.guest.weatherandroid.R;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ForecastListAdapter extends RecyclerView.Adapter<ForecastListAdapter.ForecastViewHolder> {
+    private ArrayList<Weather> mWeatherList = new ArrayList<>();
+    private Context mContext;
+
+    public ForecastListAdapter(Context context, ArrayList<Weather> weather) {
+        mContext = context;
+        mWeatherList = weather;
+    }
+
+    @Override
+    public ForecastListAdapter.ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.forecast_list_item, parent, false);
+        ForecastViewHolder viewHolder = new ForecastViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastListAdapter.ForecastViewHolder holder, int position) {
+        holder.bindWeather(mWeatherList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWeatherList.size();
+    }
+
+
+    public class ForecastViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.tempTextView)
+        TextView mTemp;
+        @Bind(R.id.dayTextView) TextView mDay;
+
+        private Context mContext;
+
+        public ForecastViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindWeather(Weather weather) {
+            mDay.setText((weather.getTime()));
+        }
+    }
+}
--- app/src/test/java/com/example/guest/weatherandroid/ExampleUnitTest.java
+++ app/src/test/java/com/example/guest/weatherandroid/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.example.guest.weatherandroid;
 
+
 import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,20 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testEpochConversion(){
+        long time = 1496275200;
+
+        Date date = new Date(time * 1000);
+
+        SimpleDateFormat format = new SimpleDateFormat("E", Locale.US);
+        String text = format.format(date);
+
+
+        assertEquals("Thu",text);
+
+
     }
 }
