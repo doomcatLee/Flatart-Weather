d636affa45969bdcdc2aae67af05a7eac80f4aee
Alex Lee <doomcat9167@gmail.com>
2017-06-11 12:23:22 -0700

    add login screen

--- app/src/main/java/com/example/guest/weatherandroid/Activities/IntroActivity.java
+++ app/src/main/java/com/example/guest/weatherandroid/Activities/IntroActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.guest.weatherandroid.R;
 
 public class IntroActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button mGetStartedButton;
+    TextView mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,19 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_intro);
         mGetStartedButton = (Button) findViewById(R.id.getStartedButton);
         mGetStartedButton.setOnClickListener(this);
+        mLogin = (TextView) findViewById(R.id.loginTextView);
+        mLogin.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == mGetStartedButton){
-            Intent intent = new Intent(IntroActivity.this, RegisterActivity.class);
+            Intent intent = new Intent(IntroActivity.this, Register1Activity.class);
+            startActivity(intent);
+        }
+
+        if (v == mLogin){
+            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
             startActivity(intent);
         }
     }
--- app/src/main/java/com/example/guest/weatherandroid/Activities/Register2Activity.java
+++ app/src/main/java/com/example/guest/weatherandroid/Activities/Register2Activity.java
@@ -1,15 +1,33 @@
 package com.example.guest.weatherandroid.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.guest.weatherandroid.R;
 
-public class Register2Activity extends AppCompatActivity {
+public class Register2Activity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        mNextButton = (TextView) findViewById(R.id.nextButton2);
+        mNextButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNextButton){
+            if (v == mNextButton){
+                Intent intent = new Intent(Register2Activity.this, ResultsActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
