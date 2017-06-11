a27dc5c8d851842c4c738b8b88f059b4519ab391
Alex Lee <doomcat9167@gmail.com>
2017-06-11 11:55:24 -0700

    add two register activities

--- app/src/main/java/com/example/guest/weatherandroid/Activities/IntroActivity.java
+++ app/src/main/java/com/example/guest/weatherandroid/Activities/IntroActivity.java
@@ -1,15 +1,30 @@
 package com.example.guest.weatherandroid.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.guest.weatherandroid.R;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button mGetStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        mGetStartedButton = (Button) findViewById(R.id.getStartedButton);
+        mGetStartedButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mGetStartedButton){
+            Intent intent = new Intent(IntroActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }
