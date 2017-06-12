65a5fcfc3f169a4c025e8041df9f981b53daa7f3
Alex Lee <doomcat9167@gmail.com>
2017-06-11 22:43:53 -0700

    not sure if i want to continue this branch, just saving it

--- app/src/main/java/com/example/guest/weatherandroid/Activities/Register1Activity.java
+++ app/src/main/java/com/example/guest/weatherandroid/Activities/Register1Activity.java
@@ -5,19 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.guest.weatherandroid.R;
 
 public class Register1Activity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView mNextButton;
+    private EditText mEmail;
+    private TextView mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
         mNextButton = (TextView) findViewById(R.id.nextButton1);
+        mEmail = (EditText) findViewById(R.id.email);
         mNextButton.setOnClickListener(this);
     }
 
@@ -26,6 +29,7 @@ public class Register1Activity extends AppCompatActivity implements View.OnClick
         if (v == mNextButton){
             if (v == mNextButton){
                 Intent intent = new Intent(Register1Activity.this, Register2Activity.class);
+                intent.putExtra("email",mEmail.getText().toString());
                 startActivity(intent);
             }
         }
--- app/src/main/java/com/example/guest/weatherandroid/Activities/Register2Activity.java
+++ app/src/main/java/com/example/guest/weatherandroid/Activities/Register2Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.guest.weatherandroid.R;
@@ -12,6 +13,8 @@ import com.example.guest.weatherandroid.R;
 public class Register2Activity extends AppCompatActivity implements View.OnClickListener{
 
     TextView mNextButton;
+    private EditText mPassword;
+    private String mEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +22,18 @@ public class Register2Activity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_register2);
         mNextButton = (TextView) findViewById(R.id.nextButton2);
         mNextButton.setOnClickListener(this);
+        mPassword = (EditText) findViewById(R.id.passwordEditText);
+        Intent intent = getIntent();
+        mEmail = intent.getStringExtra("email");
     }
 
     @Override
     public void onClick(View v) {
         if (v == mNextButton){
             if (v == mNextButton){
-                Intent intent = new Intent(Register2Activity.this, MainActivity.class);
+                Intent intent = new Intent(Register2Activity.this, Register3Activity.class);
+                intent.putExtra("password", mPassword.getText().toString());
+                intent.putExtra("email", mEmail);
                 startActivity(intent);
             }
         }
