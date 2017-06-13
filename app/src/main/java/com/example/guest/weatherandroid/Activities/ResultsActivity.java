a30bc2c8196b0ea72c54a1782a1336a44cdd0f49
Alex Lee <doomcat9167@gmail.com>
2017-06-12 21:04:48 -0700

    add more layout

--- app/src/main/java/com/example/guest/weatherandroid/Activities/ResultsActivity.java
+++ app/src/main/java/com/example/guest/weatherandroid/Activities/ResultsActivity.java
@@ -2,6 +2,8 @@ package com.example.guest.weatherandroid.Activities;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,6 +45,9 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
     private ForecastListAdapter mAdapter;
     private User mUser;
     private FloatingActionButton mSaveButton;
+
+    BottomNavigationView bottomNavigationView;
+
 
     ArrayList<Weather> mWeather = new ArrayList<>();
 
@@ -104,6 +109,30 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         String location = intent.getStringExtra("location");
 //        mSaveButton = (FloatingActionButton) findViewById(R.id.saveButton);
 //        mSaveButton.setOnClickListener(this);
+
+        bottomNavigationView  = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        if (item.getItemId() == R.id.action_home){
+//                            Intent intent1 = new Intent(ResultsActivity.this, ResultsActivity.class);
+//                            startActivity(intent1);
+                        }else if(item.getItemId() == R.id.action_data){
+                            Intent intent2 = new Intent(ResultsActivity.this, DataActivity.class);
+                            startActivity(intent2);
+                        }else if(item.getItemId() == R.id.action_search){
+                            Intent intent3 = new Intent(ResultsActivity.this, MainActivity.class);
+                            startActivity(intent3);
+                        }else{
+
+                        }
+                        return true;
+                    }
+                });
 
 
 
